de7f223a410b3bbe7eabf3441d229de56d99c87c
harish b <harish.numb19@gmail.com>
2022-11-01 18:28:48 -0400

    bug fix

--- src/Components/Appointment/ScheduleAppointment.js
+++ src/Components/Appointment/ScheduleAppointment.js
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import dayjs from 'dayjs';
 import TextField from '@mui/material/TextField';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import {format} from "date-fns";
+import { LocalizationProvider,DateTimePicker } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 const ScheduleAppointment= () => {
-    const [value, setValue] = React.useState(dayjs('2022-10-31T08:30:54'));
+    const [value, setValue] = React.useState(new Date());
 
     const handleChange = (newValue) => {
         setValue(newValue);
@@ -14,7 +13,7 @@ const ScheduleAppointment= () => {
 
     return (<>
         <h1>Schedule Appointment</h1>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
                 <DateTimePicker
                     label="Choose Date & Time "
                     value={value}
