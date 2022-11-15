4fcdf9c727d05444d9ddd1c02f7d2241a4d730d5
Shreya Mohan <92889103+smohan98@users.noreply.github.com>
2022-11-14 23:05:08 -0500

    Completed appointment scheduler

--- src/graphql/mutation.js
+++ src/graphql/mutation.js
@@ -74,3 +74,12 @@ export const UPDATE_RECORD = gql`
         }
     }
 `
+
+export const ADD_APPOINTMENT = gql`
+    mutation insertAppointmentOne($object: appointment_insert_input!) {
+      insert_appointment_one(object: $object) {
+        id
+      }
+    }
+
+`
